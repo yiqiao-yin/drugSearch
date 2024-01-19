b1147c1e0493293ac91b63e8f2eb869bf61b4db0
Yiqiao Yin <eagle0504@gmail.com>
2024-01-19 16:00:47 -0500

    add vectordb

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ openai_client = openai.OpenAI()
 # The `chromadb` variable now holds the Chroma database instance.
 db = load_vector_db("./vectorstore")
 
+query = f"User origin question: what is exenatide?"
+docs = db.similarity_search(query)
+st.write(docs)
 
 # Set the title of the Streamlit web page to "Drug Search🤖💊".
 st.title("Drug Search🤖💊")
@@ -87,50 +90,49 @@ with st.sidebar:
 clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
 
-# Initialize the chat history in the Streamlit session state if it doesn't already exist.
-# The session state persists across reruns of the app, enabling continuity in the chat.
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# # Initialize the chat history in the Streamlit session state if it doesn't already exist.
+# # The session state persists across reruns of the app, enabling continuity in the chat.
+# if "messages" not in st.session_state:
+#     st.session_state.messages = []
 
-# Clear the chat history in the Streamlit session state if it gets long or messy.
-# The session state persists across reruns of the app, enabling continuity in the chat.
-if clear_button:
-    st.session_state.messages = []
+# # Clear the chat history in the Streamlit session state if it gets long or messy.
+# # The session state persists across reruns of the app, enabling continuity in the chat.
+# if clear_button:
+#     st.session_state.messages = []
 
-# Display each message in the chat history.
-# This loop goes through all the messages saved in the session state and displays them on the web page.
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# # Display each message in the chat history.
+# # This loop goes through all the messages saved in the session state and displays them on the web page.
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
 
-# Input field for the user to enter their message.
-# 'st.chat_input' creates a chat-style input box with the prompt "What is up?"
-if prompt := st.chat_input("What is up?"):
-    # Display the user's message in a chat message container labeled as "user".
-    st.chat_message("user").markdown(prompt)
-    # Add the user's message to the chat history in the session state.
-    st.session_state.messages.append({"role": "user", "content": prompt})
+# # Input field for the user to enter their message.
+# # 'st.chat_input' creates a chat-style input box with the prompt "What is up?"
+# if prompt := st.chat_input("What is up?"):
+#     # Display the user's message in a chat message container labeled as "user".
+#     st.chat_message("user").markdown(prompt)
+#     # Add the user's message to the chat history in the session state.
+#     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Create a response string that echoes the user's input.
-    response = f"Drug Search🤖💊: {prompt}"
-    query = f"User origin question:{prompt}"
-    docs = db.similarity_search(query)
-    # retrieved_documents = " ".join([docs[i].page_content for i in range(len(docs))])
-    retrieved_documents = " "
-    response = rag(query=query, retrieved_documents=retrieved_documents)
+#     # Create a response string that echoes the user's input.
+#     response = f"Drug Search🤖💊: {prompt}"
+#     query = f"User origin question:{prompt}"
+#     docs = db.similarity_search(query)
+#     retrieved_documents = " ".join([docs[i].page_content for i in range(len(docs))])
+#     response = rag(query=query, retrieved_documents=retrieved_documents)
 
-    # Create reference table
-    # references = pd.DataFrame(
-    #     [[docs[i].metadata["source"], docs[i].page_content] for i in range(len(docs))]
-    # )
-    # if references.shape[1] >= 2:
-    #     references.columns = ["Source", "Excerpt"]
+#     # Create reference table
+#     references = pd.DataFrame(
+#         [[docs[i].metadata["source"], docs[i].page_content] for i in range(len(docs))]
+#     )
+#     if references.shape[1] >= 2:
+#         references.columns = ["Source", "Excerpt"]
 
-    # Display the assistant's response in a chat message container labeled as "assistant".
-    with st.chat_message("assistant"):
-        st.markdown(response)
-        st.write("#### Reference")
-        # st.write(references)
-        st.write(docs)
-    # Add the assistant's response to the chat history in the session state.
-    st.session_state.messages.append({"role": "assistant", "content": response})
+#     # Display the assistant's response in a chat message container labeled as "assistant".
+#     with st.chat_message("assistant"):
+#         st.markdown(response)
+#         st.write("#### Reference")
+#         st.write(references)
+#         st.write(docs)
+#     # Add the assistant's response to the chat history in the session state.
+#     st.session_state.messages.append({"role": "assistant", "content": response})
