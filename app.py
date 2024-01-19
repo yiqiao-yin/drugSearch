f46cccd9a70bb58278d4e61aa6ff57c23eb12012
Yiqiao Yin <eagle0504@gmail.com>
2024-01-19 16:14:09 -0500

    try to fix bug

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ openai_client = openai.OpenAI()
 # involving 'Exenatide', which might be a drug or a medical treatment.
 # Create a Chroma database by calling the `build_chromadb` function with the list of file names.
 # The `chromadb` variable now holds the Chroma database instance.
-db = load_vector_db("vectorstore/")
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+db = Chroma(persist_directory="./vectorstore", embedding_function=embedding_function)
 
 query = f"User origin question: what is exenatide?"
 st.write(query)
