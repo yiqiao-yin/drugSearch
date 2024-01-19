b047a8346f13a76812224bf9bc1517d682dedf1b
Yiqiao Yin <eagle0504@gmail.com>
2024-01-19 17:40:02 -0500

    dir organized

--- app.py
+++ app.py
@@ -7,33 +7,74 @@ from langchain import OpenAI, VectorDBQA
 from langchain.chains import RetrievalQAWithSourcesChain
 import PyPDF2
 
-
-# This function will go through pdf and extract and return list of page texts.
-def read_and_textify(files):
-    text_list = []
-    sources_list = []
-    for file in files:
-        pdfReader = PyPDF2.PdfReader(file)
-        # print("Page Number:", len(pdfReader.pages))
-        for i in range(len(pdfReader.pages)):
-            pageObj = pdfReader.pages[i]
-            text = pageObj.extract_text()
-            pageObj.clear()
-            text_list.append(text)
-            sources_list.append(file.name + "_page_" + str(i))
-    return [text_list, sources_list]
+from helpers.foundation_models import *
 
 
-st.set_page_config(layout="centered", page_title="Multidoc_QnA")
-st.header("Multidoc_QnA")
+st.set_page_config(layout="centered", page_title="Drug Search🤖💊")
+st.header("Drug Search🤖💊")
 st.write("---")
 
 # file uploader
 with st.sidebar:
+    with st.expander("Instruction Manual 📖"):
+        st.markdown(
+            """
+            # Exenatide Chatbot User Manual 🤖💊
+
+            Welcome to the Exenatide Chatbot, your go-to assistant for all information about the proprietary drug "Exenatide." This easy-to-use chatbot is designed to provide quick, reliable answers to your questions about Exenatide. Follow these simple steps to start chatting!
+
+            ## Getting Started 🚀
+
+            1. **Access the Chatbot**: Open the Exenatide Chatbot application on your preferred device.
+
+            2. **Start Chatting**: Simply type your question about Exenatide into the chat window. It could be anything from dosage information to side effects or general inquiries about the drug.
+
+            3. **Send Your Question**: Press the 'Send' button or hit 'Enter' to submit your question to the chatbot.
+
+            ## Chatting with Exenatide Chatbot 🤔💬
+
+            - **Ask Anything**: Whether it’s detailed drug composition, usage guidelines, storage instructions, or safety precautions, feel free to ask. Example: "What is the recommended dosage of Exenatide for adults?"
+
+            - **Use Simple Language**: For best results, use clear and concise questions. The chatbot is designed to understand everyday language.
+
+            - **Wait for the Response**: Once you submit your question, the chatbot will process it and provide an answer shortly.
+
+            - **Follow-Up Questions**: You can ask follow-up questions or new questions at any time. Just type them into the chat window and send.
+
+            ## Tips for a Better Experience ✨
+
+            - **Be Specific**: The more specific your question, the more accurate the chatbot's response will be.
+
+            - **Check for Typing Errors**: Ensure your question is free from typos to help the chatbot understand you better.
+
+            - **Emoji Use**: Feel free to use emojis in your questions! The chatbot is emoji-friendly. 😊
+
+            - **Patience is Key**: If the chatbot takes a moment to respond, don't worry. It's just processing the best possible answer for you!
+
+            ## Support and Feedback 🤝
+
+            - **Experiencing Issues?**: If you face any issues or have technical difficulties, please contact our support team.
+
+            - **We Value Your Feedback**: After using the Exenatide Chatbot, we would love to hear your thoughts. Your feedback helps us improve!
+
+            ## The Team Behind the App 🧑‍💻👩‍💻
+
+            The Exenatide Chatbot is proudly founded by Peter Yin and Yiqiao Yin. Their dedication and expertise have been instrumental in bringing this innovative solution to life.
+
+            - **Peter Yin**: Get to know more about Peter and his professional journey on [LinkedIn](https://www.linkedin.com/in/peter-yin-7914ba25/).
+
+            - **Yiqiao Yin**: Discover more about Yiqiao's background and accomplishments on [LinkedIn](https://www.linkedin.com/in/yiqiaoyin/).
+
+            Thank you for using the Exenatide Chatbot! We hope it helps you find all the information you need about Exenatide quickly and easily. Happy chatting! 🎉💬
+
+            """
+        )
+
     uploaded_files = st.file_uploader(
         "Upload documents", accept_multiple_files=True, type=["txt", "pdf"]
     )
     st.write("---")
+
 
 if uploaded_files is None:
     st.info(f"""Upload files to analyse""")
@@ -47,10 +88,12 @@ elif uploaded_files:
 
     # extract embeddings
     embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
+
     # vstore with metadata. Here we will store page numbers.
     vStore = Chroma.from_texts(
         documents, embeddings, metadatas=[{"source": s} for s in sources]
     )
+
     # deciding model
     model_name = "gpt-3.5-turbo"
     # model_name = "gpt-4"
@@ -64,6 +107,7 @@ elif uploaded_files:
         openai_api_key=st.secrets["openai_api_key"],
         streaming=True,
     )
+
     model = RetrievalQAWithSourcesChain.from_chain_type(
         llm=llm, chain_type="stuff", retriever=retriever
     )
---
+++ helpers/foundation_models.py
@@ -0,0 +1,41 @@
+from typing import List, Tuple
+import streamlit as st
+import PyPDF2
+
+
+def read_and_textify(
+    files: List[st.uploaded_file_manager.UploadedFile],
+) -> Tuple[List[str], List[str]]:
+    """
+    Reads PDF files and extracts text from each page.
+
+    This function iterates over a list of uploaded PDF files, extracts text from each page,
+    and compiles a list of texts and corresponding source information.
+
+    Args:
+    files (List[st.uploaded_file_manager.UploadedFile]): A list of uploaded PDF files.
+
+    Returns:
+    Tuple[List[str], List[str]]: A tuple containing two lists:
+        1. A list of strings, where each string is the text extracted from a PDF page.
+        2. A list of strings indicating the source of each text (file name and page number).
+    """
+
+    # Initialize lists to store extracted texts and their sources
+    text_list = []  # List to store extracted text
+    sources_list = []  # List to store source information
+
+    # Iterate over each file
+    for file in files:
+        pdfReader = PyPDF2.PdfReader(file)  # Create a PDF reader object
+        # Iterate over each page in the PDF
+        for i in range(len(pdfReader.pages)):
+            pageObj = pdfReader.pages[i]  # Get the page object
+            text = pageObj.extract_text()  # Extract text from the page
+            pageObj.clear()  # Clear the page object (optional, for memory management)
+            text_list.append(text)  # Add extracted text to the list
+            # Create a source identifier and add it to the list
+            sources_list.append(file.name + "_page_" + str(i))
+
+    # Return the lists of texts and sources
+    return [text_list, sources_list]
